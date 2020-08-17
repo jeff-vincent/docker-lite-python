e7e88cd5c996f859ce16f5837c57f0dfca862c72
Jeff Vincent <34778075+jeff-vincent@users.noreply.github.com>
2020-08-17 16:59:41 -0700

    Create docker_py.py

---
+++ docker_py.py
@@ -0,0 +1,25 @@
+import docker
+
+class DockerPy:
+
+	def __init__(self):
+		self.client = docker.from_env()
+
+	def start_container(self, image, container_name, command):
+		response = self.client.containers.run(
+			image=image, 
+			tty=True, 
+			name=container_name, 
+			command=command,
+			remove=True)
+		return response
+
+	def pass_command(self, container_name, command):
+		container = self.client.containers.get(container_name)
+		response = container.exec_run(command)
+		return response
+		
+	def kill_container(self, container_name):
+		response = self.client.containers.stop(container_name)
+		response1 = self.client.containers.remove(container_name)
+		return (response, response1)
