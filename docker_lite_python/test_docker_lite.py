225bc5ac057996abb270539bfdd5970137065984
Jeff Vincent <34778075+jeff-vincent@users.noreply.github.com>
2020-08-22 14:34:21 -0700

    Expand tests

--- docker_lite_python/test_docker_lite.py
+++ docker_lite_python/test_docker_lite.py
@@ -6,16 +6,19 @@ dl = DockerLite()
 
 def test_run():
         test_container_name = 'test-container'
-        # first check that a conainer by the 
-        # test name doesn't exist.
-        # try:
-            # if dl.get_container_by_name('test-container'):
-                # dl.kill_container('test-container')
-        # except:
-            # pass
-        response = dl.run_container('alpine', 'test-container', 'echo "Hello World!"')
-
+        response = dl.run_container('alpine', test_container_name, 'echo "Hello World!"')
         assert response == b'Hello World!\n'
+        
+def test_get():
+        container = dl.get_container_by_name('test-container')
+        assert container.name == 'test-container'
 
-        # dl.kill_container('test-container')
+def test_exec():
+        test_container_name = 'exec-container'
+        dl.run_container('alpine', test_container_name, 'sleep infinity')
+        response = dl.exec_into_running_container(test_container_name, 'echo "Hello World!"')
+        assert response == "ExecResult(exit_code=0, output=b'Hello World!\n')"
+        
+
+
 
